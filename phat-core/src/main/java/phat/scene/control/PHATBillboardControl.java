7fc35255da274aa59b6e63b5aebdbae58e5c591f
pcampillo <pabcampi@ucm.es>
2014-06-03 15:54:29 +0200

    License Header added to code

--- phat-core/src/main/java/phat/scene/control/PHATBillboardControl.java
+++ phat-core/src/main/java/phat/scene/control/PHATBillboardControl.java
@@ -1,12 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Pablo Campillo-Sanchez <pabcampi@ucm.es>
+ *
+ * This software has been developed as part of the 
+ * SociAAL project directed by Jorge J. Gomez Sanz
+ * (http://grasia.fdi.ucm.es/sociaal)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package phat.scene.control;
 
-import com.jme3.bounding.BoundingVolume;
-import com.jme3.math.FastMath;
-import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
